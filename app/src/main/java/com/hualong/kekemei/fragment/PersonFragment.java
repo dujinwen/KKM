386030d60637460152c2aa84ea0acec921889100
hjdudu <1173408486@qq.com>
2018-10-10 15:41:44 +0800

    修复切图

--- app/src/main/java/com/hualong/kekemei/fragment/PersonFragment.java
+++ app/src/main/java/com/hualong/kekemei/fragment/PersonFragment.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hualong.kekemei.R;
+import com.hualong.kekemei.Utills.CircleImageView;
+import com.hualong.kekemei.Utills.NoScrollGridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
@@ -17,9 +23,26 @@ import com.hualong.kekemei.R;
  */
 public class PersonFragment extends Fragment {
 
+    @BindView(R.id.top_bg)
+    View topBg;
+    @BindView(R.id.icon)
+    CircleImageView icon;
+
+    Unbinder unbinder;
+    @BindView(R.id.ncgv)
+    NoScrollGridView ncgv;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_person, container, false);
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
