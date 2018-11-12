d3d4208d61dfef7c20b060a24ba1c01450bdacf9
caozhimin <18210462051@163.com>
2018-11-12 17:14:56 +0800

    更新秒杀页面代码

--- app/src/main/java/com/kekemei/kekemei/activity/MiaoshaActivity.java
+++ app/src/main/java/com/kekemei/kekemei/activity/MiaoshaActivity.java
@@ -2,30 +2,41 @@ package com.kekemei.kekemei.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.jcloud.image_loader_module.ImageLoaderUtil;
 import com.kekemei.kekemei.R;
-import com.kekemei.kekemei.utils.SPUtils;
-import com.kekemei.kekemei.utils.URLs;
 import com.kekemei.kekemei.adapter.MiaoshaPagerAdapter;
 import com.kekemei.kekemei.bean.MiaoshaBean;
+import com.kekemei.kekemei.fragment.IPage;
+import com.kekemei.kekemei.fragment.MiaoshaFragment;
+import com.kekemei.kekemei.utils.CollectionUtils;
+import com.kekemei.kekemei.utils.LogUtil;
+import com.kekemei.kekemei.utils.URLs;
+import com.kekemei.kekemei.view.MultipleStatusView;
 import com.kekemei.kekemei.view.PagerSlidingTabStrip;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 import com.lzy.okgo.model.Response;
 import com.stx.xhb.xbanner.XBanner;
 
+import java.util.List;
+
 import butterknife.BindView;
 
 /**
  * 抢购页面
  */
-public class MiaoshaActivity extends BaseActivity {
+public class MiaoshaActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
     public static final String TAG = MiaoshaActivity.class.getSimpleName();
     public static final String KEY_SKU_ID = "item_id";
     public static final String KEY_GROUP_ID = "groupId";
@@ -41,6 +52,11 @@ public class MiaoshaActivity extends BaseActivity {
     PagerSlidingTabStrip miaoshaTabs;
     @BindView(R.id.rushHallPager)
     ViewPager pagers;
+
+    @BindView(R.id.multiple_status_view)
+    MultipleStatusView multipleStatusView;
+    @BindView(R.id.contentLayout)
+    LinearLayout contentLayout;
 
     private MiaoshaPagerAdapter adapter;//抢购页面ViewPager的adapter
 
@@ -72,7 +88,7 @@ public class MiaoshaActivity extends BaseActivity {
     protected void initView(Bundle savedInstanceState) {
         super.initView(savedInstanceState);
         toolbar.setNavigationIcon(R.mipmap.back);
-        TextView tv_title = toolbar.findViewById(R.id.tv_title);
+        toolbar.setBackgroundColor(Color.parseColor("#10000000"));
         tv_title.setText("秒杀专场");
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -82,23 +98,76 @@ public class MiaoshaActivity extends BaseActivity {
             }
         });
 
+        multipleStatusView.setOnRetryClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initData();
+            }
+        });
+
+        multipleStatusView.showOutContentView(contentLayout);
+
+        adapter = new MiaoshaPagerAdapter(this, getSupportFragmentManager());
+        pagers.addOnPageChangeListener(this);
+
     }
 
     @Override
     protected void initData() {
         super.initData();
-        String latitude = SPUtils.getString(this, "latitude", "");
-        String longitude = SPUtils.getString(this, "longitude", "");
-        OkGo.<String>post(URLs.SECOND).params("longitude", longitude)
-                .params("latitude", latitude).execute(new StringCallback() {
+        multipleStatusView.showLoading();
+        OkGo.<String>post(URLs.SECOND).params("id", "1").execute(new StringCallback() {
             @Override
             public void onSuccess(Response<String> response) {
+                multipleStatusView.showOutContentView(contentLayout);
+                LogUtil.e("miaosha", "body:" + response.body());
                 Gson gson = new Gson();
                 MiaoshaBean miaoShaBean = gson.fromJson(response.body(), MiaoshaBean.class);
-//                xbanner.setData(miaoShaBean.getData().getBanneradv(), null);
+                xbanner.setData(miaoShaBean.getData().getBanner(), null);
                 initBanner();
+                fillData(miaoShaBean.getData().getSecond());
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+                multipleStatusView.showError();
             }
         });
+    }
+
+    private void fillData(List<MiaoshaBean.DataBean.SecondBean> tabsBean) {
+        adapter.clear();
+        if (CollectionUtils.isEmpty(tabsBean)) {
+            return;
+        }
+
+        for (int i = 0; i < tabsBean.size(); i++) {
+            MiaoshaBean.DataBean.SecondBean tab = tabsBean.get(i);
+            if (tab != null) {
+//                String tabText = tab.getTimeTxt() + "\n" + tab.getStatusTxt();
+                if (i == 0) {
+                    String tabText = tab.getName() + "\n" + "抢购进行中";
+                    adapter.addTitle(tabText);
+                } else {
+                    String tabText = tab.getName() + "\n" + "即将开始";
+                    adapter.addTitle(tabText);
+                }
+                boolean lastGroup = (i == tabsBean.size() - 1);
+                adapter.addFragment(MiaoshaFragment.newInstance(tabsBean.get(i).getProject_data()));
+            }
+        }
+
+        // http://www.cnblogs.com/sklww/p/3361074.html
+        // 如果adapter被set到ViewPager之后，数据有变化的话，需要在每次setAdapter之前调用notifyDataSetChanged
+        adapter.notifyDataSetChanged();
+        pagers.setAdapter(adapter);
+        pagers.setOffscreenPageLimit(tabsBean.size() - 1);
+        miaoshaTabs.setViewPager(pagers);
+
+        pagers.setCurrentItem(curPosition, true);
+        // 第一次进入秒杀列表的时候在onResume时还未初始化Fragment，需要在这里调用setOnEnter
+        setOnEnter(curPosition);
     }
 
     /**
@@ -118,8 +187,78 @@ public class MiaoshaActivity extends BaseActivity {
             public void loadBanner(XBanner banner, Object model, View view, int position) {
                 //在此处使用图片加载框架加载图片，demo中使用glide加载，可替换成自己项目中的图片加载框架
                 //                Glide.with(MainActivity.this).load(((AdvertiseEntity.OthersBean) model).getThumbnail()).placeholder(R.drawable.default_image).error(R.drawable.default_image).into((ImageView) view);
-//                ImageLoaderUtil.getInstance().loadImage(URLs.BASE_URL + ((HomeBean.DataBean.BanneradvBean) model).getImage(), (ImageView) view);
+                ImageLoaderUtil.getInstance().loadImage(URLs.BASE_URL + ((MiaoshaBean.DataBean.BannerBean) model).getImage(), (ImageView) view);
             }
         });
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        setOnLeave(curPosition);
+        curPosition = position;
+        setOnEnter(curPosition);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    /**
+     * 通知指定页触发离开页面事件。页面要实现IPage接口
+     *
+     * @param position 页的索引
+     */
+    private void setOnLeave(int position) {
+        LogUtil.d(TAG, "setOnLeave position is " + position);
+        Fragment fragment = getCurrentFragment(position);
+        if (fragment != null && fragment instanceof IPage) {
+            IPage page = (IPage) fragment;
+            page.onLeave();
+        }
+    }
+
+    /**
+     * 通知指定页触发进入事件。页面要实现IPage接口
+     *
+     * @param position 页的索引
+     */
+    private void setOnEnter(int position) {
+        LogUtil.d(TAG, "setOnEnter position is " + position);
+        Fragment fragment = getCurrentFragment(position);
+        if (fragment != null && fragment instanceof IPage) {
+            IPage page = (IPage) fragment;
+            page.onEnter();
+        }
+    }
+
+    private Fragment getCurrentFragment(int position) {
+        try {
+            if (pagers == null || pagers.getAdapter() == null) {
+                return null;
+            }
+            MiaoshaPagerAdapter homeAdapter = (MiaoshaPagerAdapter) pagers.getAdapter();
+            return homeAdapter.getItem(position);
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage(), e);
+            return null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setOnEnter(curPosition);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setOnLeave(curPosition);
     }
 }
--- app/src/main/java/com/kekemei/kekemei/bean/MiaoshaBean.java
+++ app/src/main/java/com/kekemei/kekemei/bean/MiaoshaBean.java
@@ -1,20 +1,22 @@
 package com.kekemei.kekemei.bean;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class MiaoshaBean {
 
     /**
      * code : 1
+     * data : {"banner":[{"adv_pos_id":10344,"becautician_becautician_id":0,"content":"杀Banner广告","createtime":1539694959,"endtime":"2019-10-16","id":3,"image":"/uploads/20181022/4208428115e55145e7ca26f73fa5c6d3.jpg","jumbdata":"project","jumbdata_text":"Jumbdata project","project_project_id":4,"shop_shop_id":0,"starttime":"2018-10-16","switch":1,"title":"杀Banner广告","url":"","views":0,"weigh":0},{"adv_pos_id":10346,"becautician_becautician_id":0,"content":"秒杀广告","createtime":1539697575,"endtime":"2019-12-28","id":4,"image":"/uploads/20181022/0847d20fcb22b348e29a61e333a363b4.jpg","jumbdata":"web","jumbdata_text":"Jumbdata web","project_project_id":0,"shop_shop_id":0,"starttime":"2018-10-16","switch":1,"title":"秒杀广告","url":"","views":0,"weigh":0}],"second":[{"endtimes":"13:00:00","id":1,"name":"12:00专场","project_data":[{"content":"1111","coupon_ids":"1","createtime":1538452488,"id":3,"image":"/uploads/20181022/eba3d6f9ffc2bd7f7791f407bccec354.jpg","images":"/uploads/20181022/979c9ab660275292aefc5c6b7632b2a8.jpg,/uploads/20181022/5e50668033beb8d9d671da58b598fa79.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg","name":"金因姜时光肌密面膜","price_discount":66,"price_market":666,"price_member":555,"price_newmember":0,"price_second":0,"project_category_id":1,"quality":"","quality_text":"","redenvelopes_ids":"1","state":"1","state_text":"正常","treatment_count":"0","treatment_price":"55.00","views":0,"weigh":100},{"content":"55","coupon_ids":"1","createtime":1538487574,"id":4,"image":"/uploads/20181022/0847d20fcb22b348e29a61e333a363b4.jpg","images":"/uploads/20181022/b532838d95d86275310107d512861b6c.jpg,/uploads/20181022/5e50668033beb8d9d671da58b598fa79.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg","name":"粉红丝带","price_discount":100,"price_market":0,"price_member":0,"price_newmember":33,"price_second":0,"project_category_id":1,"quality":"","quality_text":"","redenvelopes_ids":"1","state":"1","state_text":"正常","treatment_count":"3","treatment_price":"44.00","views":0,"weigh":4}],"project_project_ids":"3,4","starttimes":"12:00:00","state":"1","state_text":"正常"}]}
      * msg : 请求成功
-     * time : 1539769512
-     * data : {"second":[{"id":1,"name":"12:00专场","starttimes":"12:00:00","endtimes":"13:00:00","project_project_ids":"3,4","state":"1","project_data":[{"id":3,"name":"会员区","project_category_id":2,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","images":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","price":665.83,"price_market":0,"price_discount":0,"treatment_price":"55.00","price_newmember":0,"price_member":555,"price_second":0,"treatment_count":"0","quality":"","activitytime":"2018-10-02 11:53:50","activitytime_end":"2018-10-02 11:53:50","content":"1111","flag":"","coupon_ids":null,"state":"1","weigh":100,"views":0,"createtime":1538452488,"quality_text":"","flag_text":"","state_text":"正常"},{"id":4,"name":"新人","project_category_id":1,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","images":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","price":55,"price_market":0,"price_discount":0,"treatment_price":"44.00","price_newmember":33,"price_member":0,"price_second":0,"treatment_count":"3","quality":"","activitytime":"2018-10-02 21:38:44","activitytime_end":"2018-10-02 21:38:44","content":"55","flag":"","coupon_ids":null,"state":"1","weigh":4,"views":0,"createtime":1538487574,"quality_text":"","flag_text":"","state_text":"正常"}],"state_text":"正常"}],"banner":[{"id":3,"title":"杀Banner广告","adv_pos_id":10344,"content":"杀Banner广告","views":0,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","starttime":"2018-10-16 21:01:31","endtime":"2018-10-16 21:01:31","createtime":1539694959,"switch":1},{"id":4,"title":"秒杀广告","adv_pos_id":10346,"content":"秒杀广告","views":0,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","starttime":"2018-10-16 21:45:46","endtime":"2018-10-16 21:45:46","createtime":1539697575,"switch":1}]}
+     * time : 1542011424
      */
 
     private int code;
+    private DataBean data;
     private String msg;
     private String time;
-    private DataBean data;
 
     public int getCode() {
         return code;
@@ -22,6 +24,14 @@ public class MiaoshaBean {
 
     public void setCode(int code) {
         this.code = code;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
     }
 
     public String getMsg() {
@@ -40,25 +50,9 @@ public class MiaoshaBean {
         this.time = time;
     }
 
-    public DataBean getData() {
-        return data;
-    }
-
-    public void setData(DataBean data) {
-        this.data = data;
-    }
-
     public static class DataBean {
-        private List<SecondBean> second;
         private List<BannerBean> banner;
-
-        public List<SecondBean> getSecond() {
-            return second;
-        }
-
-        public void setSecond(List<SecondBean> second) {
-            this.second = second;
-        }
+        private List<SecondBean> second;
 
         public List<BannerBean> getBanner() {
             return banner;
@@ -68,26 +62,219 @@ public class MiaoshaBean {
             this.banner = banner;
         }
 
+        public List<SecondBean> getSecond() {
+            return second;
+        }
+
+        public void setSecond(List<SecondBean> second) {
+            this.second = second;
+        }
+
+        public static class BannerBean {
+            /**
+             * adv_pos_id : 10344
+             * becautician_becautician_id : 0
+             * content : 杀Banner广告
+             * createtime : 1539694959
+             * endtime : 2019-10-16
+             * id : 3
+             * image : /uploads/20181022/4208428115e55145e7ca26f73fa5c6d3.jpg
+             * jumbdata : project
+             * jumbdata_text : Jumbdata project
+             * project_project_id : 4
+             * shop_shop_id : 0
+             * starttime : 2018-10-16
+             * switch : 1
+             * title : 杀Banner广告
+             * url :
+             * views : 0
+             * weigh : 0
+             */
+
+            private int adv_pos_id;
+            private int becautician_becautician_id;
+            private String content;
+            private int createtime;
+            private String endtime;
+            private int id;
+            private String image;
+            private String jumbdata;
+            private String jumbdata_text;
+            private int project_project_id;
+            private int shop_shop_id;
+            private String starttime;
+            @SerializedName("switch")
+            private int switchX;
+            private String title;
+            private String url;
+            private int views;
+            private int weigh;
+
+            public int getAdv_pos_id() {
+                return adv_pos_id;
+            }
+
+            public void setAdv_pos_id(int adv_pos_id) {
+                this.adv_pos_id = adv_pos_id;
+            }
+
+            public int getBecautician_becautician_id() {
+                return becautician_becautician_id;
+            }
+
+            public void setBecautician_becautician_id(int becautician_becautician_id) {
+                this.becautician_becautician_id = becautician_becautician_id;
+            }
+
+            public String getContent() {
+                return content;
+            }
+
+            public void setContent(String content) {
+                this.content = content;
+            }
+
+            public int getCreatetime() {
+                return createtime;
+            }
+
+            public void setCreatetime(int createtime) {
+                this.createtime = createtime;
+            }
+
+            public String getEndtime() {
+                return endtime;
+            }
+
+            public void setEndtime(String endtime) {
+                this.endtime = endtime;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getImage() {
+                return image;
+            }
+
+            public void setImage(String image) {
+                this.image = image;
+            }
+
+            public String getJumbdata() {
+                return jumbdata;
+            }
+
+            public void setJumbdata(String jumbdata) {
+                this.jumbdata = jumbdata;
+            }
+
+            public String getJumbdata_text() {
+                return jumbdata_text;
+            }
+
+            public void setJumbdata_text(String jumbdata_text) {
+                this.jumbdata_text = jumbdata_text;
+            }
+
+            public int getProject_project_id() {
+                return project_project_id;
+            }
+
+            public void setProject_project_id(int project_project_id) {
+                this.project_project_id = project_project_id;
+            }
+
+            public int getShop_shop_id() {
+                return shop_shop_id;
+            }
+
+            public void setShop_shop_id(int shop_shop_id) {
+                this.shop_shop_id = shop_shop_id;
+            }
+
+            public String getStarttime() {
+                return starttime;
+            }
+
+            public void setStarttime(String starttime) {
+                this.starttime = starttime;
+            }
+
+            public int getSwitchX() {
+                return switchX;
+            }
+
+            public void setSwitchX(int switchX) {
+                this.switchX = switchX;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public int getViews() {
+                return views;
+            }
+
+            public void setViews(int views) {
+                this.views = views;
+            }
+
+            public int getWeigh() {
+                return weigh;
+            }
+
+            public void setWeigh(int weigh) {
+                this.weigh = weigh;
+            }
+        }
+
         public static class SecondBean {
             /**
+             * endtimes : 13:00:00
              * id : 1
              * name : 12:00专场
-             * starttimes : 12:00:00
-             * endtimes : 13:00:00
+             * project_data : [{"content":"1111","coupon_ids":"1","createtime":1538452488,"id":3,"image":"/uploads/20181022/eba3d6f9ffc2bd7f7791f407bccec354.jpg","images":"/uploads/20181022/979c9ab660275292aefc5c6b7632b2a8.jpg,/uploads/20181022/5e50668033beb8d9d671da58b598fa79.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg","name":"金因姜时光肌密面膜","price_discount":66,"price_market":666,"price_member":555,"price_newmember":0,"price_second":0,"project_category_id":1,"quality":"","quality_text":"","redenvelopes_ids":"1","state":"1","state_text":"正常","treatment_count":"0","treatment_price":"55.00","views":0,"weigh":100},{"content":"55","coupon_ids":"1","createtime":1538487574,"id":4,"image":"/uploads/20181022/0847d20fcb22b348e29a61e333a363b4.jpg","images":"/uploads/20181022/b532838d95d86275310107d512861b6c.jpg,/uploads/20181022/5e50668033beb8d9d671da58b598fa79.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg,/uploads/20181022/b532838d95d86275310107d512861b6c.jpg","name":"粉红丝带","price_discount":100,"price_market":0,"price_member":0,"price_newmember":33,"price_second":0,"project_category_id":1,"quality":"","quality_text":"","redenvelopes_ids":"1","state":"1","state_text":"正常","treatment_count":"3","treatment_price":"44.00","views":0,"weigh":4}]
              * project_project_ids : 3,4
+             * starttimes : 12:00:00
              * state : 1
-             * project_data : [{"id":3,"name":"会员区","project_category_id":2,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","images":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","price":665.83,"price_market":0,"price_discount":0,"treatment_price":"55.00","price_newmember":0,"price_member":555,"price_second":0,"treatment_count":"0","quality":"","activitytime":"2018-10-02 11:53:50","activitytime_end":"2018-10-02 11:53:50","content":"1111","flag":"","coupon_ids":null,"state":"1","weigh":100,"views":0,"createtime":1538452488,"quality_text":"","flag_text":"","state_text":"正常"},{"id":4,"name":"新人","project_category_id":1,"image":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","images":"/uploads/20181001/8cf03f354750e3e38664485e647c4af4.jpeg","price":55,"price_market":0,"price_discount":0,"treatment_price":"44.00","price_newmember":33,"price_member":0,"price_second":0,"treatment_count":"3","quality":"","activitytime":"2018-10-02 21:38:44","activitytime_end":"2018-10-02 21:38:44","content":"55","flag":"","coupon_ids":null,"state":"1","weigh":4,"views":0,"createtime":1538487574,"quality_text":"","flag_text":"","state_text":"正常"}]
              * state_text : 正常
              */
 
+            private String endtimes;
             private int id;
             private String name;
-            private String starttimes;
-            private String endtimes;
             private String project_project_ids;
+            private String starttimes;
             private String state;
             private String state_text;
             private List<BaseBean> project_data;
+
+            public String getEndtimes() {
+                return endtimes;
+            }
+
+            public void setEndtimes(String endtimes) {
+                this.endtimes = endtimes;
+            }
 
             public int getId() {
                 return id;
@@ -105,28 +292,20 @@ public class MiaoshaBean {
                 this.name = name;
             }
 
-            public String getStarttimes() {
-                return starttimes;
-            }
-
-            public void setStarttimes(String starttimes) {
-                this.starttimes = starttimes;
-            }
-
-            public String getEndtimes() {
-                return endtimes;
-            }
-
-            public void setEndtimes(String endtimes) {
-                this.endtimes = endtimes;
-            }
-
             public String getProject_project_ids() {
                 return project_project_ids;
             }
 
             public void setProject_project_ids(String project_project_ids) {
                 this.project_project_ids = project_project_ids;
+            }
+
+            public String getStarttimes() {
+                return starttimes;
+            }
+
+            public void setStarttimes(String starttimes) {
+                this.starttimes = starttimes;
             }
 
             public String getState() {
