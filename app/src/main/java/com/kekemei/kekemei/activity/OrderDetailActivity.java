2136a3ce8c67002f2111662b4327f9b132cce05a
hjdudu <1173408486@qq.com>
2018-11-22 14:59:17 +0800

    HUnaxin

---
+++ app/src/main/java/com/kekemei/kekemei/activity/OrderDetailActivity.java
@@ -0,0 +1,67 @@
+package com.kekemei.kekemei.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kekemei.kekemei.R;
+import com.kekemei.kekemei.utils.AppUtil;
+import com.kekemei.kekemei.view.SectionRowView;
+
+import butterknife.BindView;
+
+/**
+ * 设置页面
+ */
+public class OrderDetailActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+    @BindView(R.id.txtSafely)
+    SectionRowView safelySetting;
+//    @BindView(R.id.txtFeedBack)
+//    SectionRowView feedBack;
+    @BindView(R.id.txtHelpCenter)
+    SectionRowView helpCenter;
+    @BindView(R.id.txtAboutUs)
+    SectionRowView aboutUs;
+    @BindView(R.id.txtCleanMemory)
+    SectionRowView cleanMemory;
+    @BindView(R.id.txtVersion)
+    SectionRowView version;
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, OrderDetailActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected View setTitleBar() {
+        return toolbar;
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
+        toolbar.setNavigationIcon(R.mipmap.back);
+        tv_title.setText("订单详情");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
