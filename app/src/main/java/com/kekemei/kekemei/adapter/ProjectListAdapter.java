20b1242849d486ef41acc695d13a97b70dc5eeba
caozhimin <18210462051@163.com>
2018-11-27 13:51:23 +0800

    更新代码

--- app/src/main/java/com/kekemei/kekemei/adapter/ProjectListAdapter.java
+++ app/src/main/java/com/kekemei/kekemei/adapter/ProjectListAdapter.java
@@ -7,10 +7,11 @@ import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.jcloud.image_loader_module.ImageLoaderUtil;
 import com.kekemei.kekemei.R;
 import com.kekemei.kekemei.bean.BaseBean;
+import com.kekemei.kekemei.utils.CollectionUtils;
 import com.kekemei.kekemei.utils.URLs;
-import com.jcloud.image_loader_module.ImageLoaderUtil;
 
 public class ProjectListAdapter extends BaseQuickAdapter<BaseBean, BaseViewHolder> {
     private Context jContext;
@@ -30,12 +31,18 @@ public class ProjectListAdapter extends BaseQuickAdapter<BaseBean, BaseViewHolde
         helper.setText(R.id.tv_old_price, "¥ " + item.getPrice_market());
         helper.setVisible(R.id.ll_youhuiquan, true);
         ImageLoaderUtil.getInstance().loadImage(URLs.BASE_URL + item.getImage(), (ImageView) helper.getView(R.id.iv_order_icon));
-        if (item.getCoupon().size()==0)
-            helper.setVisible(R.id.tv_man,false);
-        helper.setText(R.id.tv_man,item.getCoupon().get(0));
-        if (item.getRedenvelopes().size() == 0)
-            helper.setVisible(R.id.tv_jian,false);
-        helper.setText(R.id.tv_jian,item.getCoupon().get(0));
+        if (CollectionUtils.isEmpty(item.getFull())) {
+            helper.setVisible(R.id.tv_man, false);
+        } else {
+            helper.setVisible(R.id.tv_man, true);
+            helper.setText(R.id.tv_man, item.getFull().get(0));
+        }
+        if (CollectionUtils.isEmpty(item.getRedenvelopes())) {
+            helper.setVisible(R.id.tv_jian, false);
+        } else {
+            helper.setVisible(R.id.tv_jian, true);
+            helper.setText(R.id.tv_jian, item.getRedenvelopes().get(0));
+        }
     }
 }
 
